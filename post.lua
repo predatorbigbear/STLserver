9b30cf3c35f0a3844badd78e1870e00b3de05194
predatorbigbear <1075716088@qq.com>
2025-07-07 23:39:42 +0800

    更新测试post.lua

--- post.lua
+++ post.lua
@@ -1,5 +1,5 @@
 wrk.method = "POST"
 wrk.headers["Content-Type"] = "application/x-www-form-urlencoded"
 wrk.headers["Host"] = "www.baidu.com:81"
-wrk.headers["Transfer-Encoding"] = " gzip, chunked, deflate"
+wrk.headers["Transfer-Encoding"] = " gzip, identity, deflate"
 wrk.headers["Connection"] = "kl ,keep-alive"
