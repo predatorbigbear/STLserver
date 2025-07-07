f1ebba3454ae29f3ba9f8d3a6ca20fed6cabf350
predatorbigbear <1075716088@qq.com>
2025-07-07 14:37:24 +0800

    用于wrk接口测试  带有解析字段的文件

---
+++ post.lua
@@ -0,0 +1,4 @@
+wrk.method = "POST"
+wrk.headers["Content-Type"] = "application/x-www-form-urlencoded"
+wrk.headers["Host"] = "www.baidu.com:81"
+wrk.headers["Transfer-Encoding"] = " gzip, chunked, deflate"
