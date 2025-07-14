f713c37396545989846a2232db05200defd3ae1d
predatorbigbear <1075716088@qq.com>
2025-07-14 18:53:13 +0800

    接口21  22  wrk执行文件

---
+++ post22.lua
@@ -0,0 +1,5 @@
+wrk.method = "POST"
+wrk.headers["Content-Type"] = "application/x-www-form-urlencoded"
+wrk.headers["Host"] = "www.baidu.com:81"
+wrk.headers["Transfer-Encoding"] = " gzip, identity, deflate"
+wrk.headers["Connection"] = "kl ,keep-alive"
