dcac46992c4b87712660ee237c98d458fbfcaa60
predatorbigbear <1075716088@qq.com>
2025-07-20 20:52:41 +0800

    上传json生成函数wrk测试文件

---
+++ post1911.lua
@@ -0,0 +1,7 @@
+wrk.method = "POST"
+wrk.headers["Content-Type"] = "application/x-www-form-urlencoded"
+wrk.headers["Host"] = "www.baidu.com:81"
+wrk.headers["Transfer-Encoding"] = " gzip, identity, deflate"
+wrk.headers["Connection"] = "kl ,keep-alive"
+
+wrk.body = "jsonType=array"
