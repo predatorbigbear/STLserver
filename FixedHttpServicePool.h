493ba1e5d5f02f1da5dfa137a551794b8b8af235
predatorbigbear <1075716088@qq.com>
2025-08-02 22:17:32 +0800

    优化代码

---
+++ FixedHttpServicePool.h
@@ -0,0 +1,73 @@
+﻿#pragma once
+
+
+#include "spinLock.h"
+#include "readBuffer.h"
+#include "httpService.h"
+#include "ASYNCLOG.h"
+#include "logPool.h"
+#include "multiSqlReadSWPool.h"
+#include "multiRedisReadPool.h"
+#include "multiRedisWritePool.h"
+#include "multiSqlWriteSWPool.h"
+
+
+struct FixedHTTPSERVICEPOOL
+{
+	FixedHTTPSERVICEPOOL(std::shared_ptr<IOcontextPool> ioPool, const std::string &doc_root,
+		std::shared_ptr<MULTISQLREADSWPOOL>multiSqlReadSWPoolMaster, 
+		std::shared_ptr<MULTIREDISREADPOOL>multiRedisReadPoolMaster,
+		std::shared_ptr<MULTIREDISWRITEPOOL>multiRedisWritePoolMaster, std::shared_ptr<MULTISQLWRITESWPOOL>multiSqlWriteSWPoolMaster,
+	    std::shared_ptr<std::function<void()>>reAccept, std::shared_ptr<LOGPOOL> logPool, std::shared_ptr<STLTimeWheel> timeWheel,
+		const std::shared_ptr<std::unordered_map<std::string_view, std::string>>fileMap,
+		const unsigned int timeOut, const std::shared_ptr<std::function<void(std::shared_ptr<HTTPSERVICE>&)>>& cleanFun,
+		int beginSize = 200);
+
+	void getNextBuffer(std::shared_ptr<HTTPSERVICE> &outBuffer);
+
+	void getBackElem(std::shared_ptr<HTTPSERVICE> &buffer);
+
+	void setNotifyAccept();
+
+	bool ready();
+
+
+
+private:
+	const std::shared_ptr<std::function<void(std::shared_ptr<HTTPSERVICE>&)>>  m_cleanFun{};
+
+	std::shared_ptr<io_context> m_ioc{};
+
+	SpinLock m_listMutex;
+
+	const std::string &m_doc_root;
+
+	std::unique_ptr<std::shared_ptr<HTTPSERVICE>[]> m_bufferList{};
+	std::shared_ptr<HTTPSERVICE> *m_iterNow{};
+	std::shared_ptr<HTTPSERVICE> *m_iterEnd{};
+	std::shared_ptr<STLTimeWheel> m_timeWheel{};
+	const std::shared_ptr<std::unordered_map<std::string_view, std::string>>m_fileMap{};
+
+	bool m_success{};
+
+	int i{};
+
+	unsigned int m_timeOut{};
+	unsigned int m_beginSize{};
+	std::shared_ptr<std::function<void()>> m_reAccept{};
+
+	std::shared_ptr<ASYNCLOG> m_log{};
+	std::shared_ptr<LOGPOOL>m_logPool{};
+	std::shared_ptr<IOcontextPool> m_ioPool{};
+
+	std::shared_ptr<MULTISQLWRITESWPOOL>m_multiSqlWriteSWPoolMaster{};         //   主数据库写入连接池
+	std::shared_ptr<MULTISQLREADSWPOOL>m_multiSqlReadSWPoolMaster{};           //   主数据库读取连接池
+
+	std::shared_ptr<MULTIREDISWRITEPOOL>m_multiRedisWritePoolMaster{};         //   主redis写入连接池
+	std::shared_ptr<MULTIREDISREADPOOL>m_multiRedisReadPoolMaster{};           //   主redis读取连接池
+
+	bool m_startAccept{ true };
+
+	std::shared_ptr<HTTPSERVICE> m_temp{};
+
+};
---
+++ readBuffer.cpp
@@ -0,0 +1,96 @@
+﻿#include "readBuffer.h"
+
+ReadBuffer::ReadBuffer(std::shared_ptr<boost::asio::io_context> ioc, const unsigned int bufNum)
+{
+	try
+	{
+		if (!ioc)
+			throw std::runtime_error("io_context is null");
+		if(!bufNum)
+			throw std::runtime_error("bufNum is 0");
+		m_sock.reset(new boost::asio::ip::tcp::socket(*ioc));
+		m_sock->set_option(boost::asio::socket_base::keep_alive(true), m_err);
+	    //https://www.cnblogs.com/xiao-tao/p/9718017.html
+		m_readBuffer.reset(new char[bufNum]);
+	}
+	catch (const std::exception &e)
+	{
+		std::cout << e.what() << "  ,please restart server\n";
+	}
+}
+
+
+char * ReadBuffer::getBuffer()
+{
+	return m_readBuffer.get();
+}
+
+size_t & ReadBuffer::getHasRead()
+{
+	// TODO: 在此处插入 return 语句
+	return m_hasReadLen;
+}
+
+MYREQVIEW & ReadBuffer::getView()
+{
+	// TODO: 在此处插入 return 语句
+	return reqView;
+}
+
+
+std::shared_ptr<boost::asio::ip::tcp::socket>& ReadBuffer::getSock()
+{
+	return m_sock;
+}
+
+boost::system::error_code & ReadBuffer::getErrCode()
+{
+	// TODO: 在此处插入 return 语句
+	return m_err;
+}
+
+
+void ReadBuffer::setBodyLen(const int len)
+{
+	m_bodyLen = len;
+}
+
+int ReadBuffer::getBodyLen()
+{
+	return m_bodyLen;
+}
+
+void ReadBuffer::setBodyFrontLen(const int len)
+{
+	m_bodyFrontLen = len;
+}
+
+int ReadBuffer::getBodyFrontLen()
+{
+	return m_bodyFrontLen;
+}
+
+const char ** ReadBuffer::bodyPara()
+{
+	return m_bodyPara.get();
+}
+
+size_t ReadBuffer::getBodyParaLen()
+{
+	return m_bodyParaLen;
+}
+
+void ReadBuffer::setFileName(const char * ch, const size_t len)
+{
+	std::string_view temp{ ch,len };
+	m_fileName.swap(temp);
+}
+
+std::string_view ReadBuffer::fileName()
+{
+	return m_fileName;
+}
+
+
+
+
